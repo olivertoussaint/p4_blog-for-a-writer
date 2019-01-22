ab17292f915a7ae6a49a43c18ac590c895d55be4
Oliver Toussaint <toussaint.olivier66@gmail.com>
2019-01-22 01:39:56 +0100

    database init

--- index.php
+++ index.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
 <html lang="fr">
-<?php
-// Sous WAMP (Windows)
-$bdd = new PDO('mysql:host=localhost;dbname=blog_jean_forteroche;charset=utf8', 'root', '');
-?>
 <head>
 	<meta charset="UTF-8">
 	<title>Blog de Jean Forteroche</title>
@@ -11,5 +7,39 @@ $bdd = new PDO('mysql:host=localhost;dbname=blog_jean_forteroche;charset=utf8', 
 </head>
 <body>
 	<h1>Billet simple pour l'Alaska</h1>
+	<?php
+try
+{
+	// On se connecte à MySQL
+	$bdd = new PDO('mysql:host=localhost;dbname=blog_jean_forteroche;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+	// En cas d'erreur, on affiche un message et on arrête tout
+        die('Erreur : '.$e->getMessage());
+}
+
+// Si tout va bien, on peut continuer
+
+// On récupère tout le contenu de la table jeux_video
+$reponse = $bdd->query('SELECT * FROM jeux_video');
+
+// On affiche chaque entrée une à une
+while ($donnees = $reponse->fetch())
+{
+?>
+    <p>
+    <strong>Jeu</strong> : <?php echo $donnees['nom']; ?><br />
+    Le possesseur de ce jeu est : <?php echo $donnees['possesseur']; ?>, et il le vend à <?php echo $donnees['prix']; ?> euros !<br />
+    Ce jeu fonctionne sur <?php echo $donnees['console']; ?> et on peut y jouer à <?php echo $donnees['nbre_joueurs_max']; ?> au maximum<br />
+    <?php echo $donnees['possesseur']; ?> a laissé ces commentaires sur <?php echo $donnees['nom']; ?> : <em><?php echo $donnees['commentaires']; ?></em>
+   </p>
+<?php
+}
+
+$reponse->closeCursor(); // Termine le traitement de la requête
+
+?>
+
 </body>
-</html>git log
+</html>
