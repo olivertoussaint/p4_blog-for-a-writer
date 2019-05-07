bf2ac5384acb213e7891bbe011961c703b28f89d
Oliver Toussaint <toussaint.olivier66@gmail.com>
2019-05-07 23:04:41 +0200

    model 1.0

---
+++ model.php
@@ -0,0 +1,20 @@
+<?php
+function getPosts()
+{
+ try
+ {
+    $db = new PDO('mysql:host=localhost;dbname=blog_jean_forteroche;charset=utf8', 'root', '');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+ }
+ catch (Exception $e)
+{
+   // En cas d'erreur, on affiche un message et on arrête tout
+         die('Erreur : ' . $e->getMessage());
+ }
+   // on récupère les posts
+       $req = $bdd->query('SELECT id, blog_post_title, blog_content, DATE_FORMAT (blog_post_date, \'%d/%m/%Y à %Hh%imin%ss\') AS blog_post_date_fr FROM blog_posts ORDER BY id DESC LIMIT 0,5');
+
+return $req;
+}
+ ?>
