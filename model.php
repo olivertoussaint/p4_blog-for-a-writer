946f026e1e1976591221ab4dfac7d11f660709f8
Oliver Toussaint <toussaint.olivier66@gmail.com>
2019-05-11 02:05:50 +0200

    model modif

--- model.php
+++ model.php
@@ -4,7 +4,6 @@ function getPosts()
  try
  {
     $db = new PDO('mysql:host=localhost;dbname=blog_jean_forteroche;charset=utf8', 'root', '');
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
  }
  catch (Exception $e)
@@ -17,4 +16,38 @@ function getPosts()
 
 return $req;
 }
- ?>
+
+function getPost($postId)
+{
+	try
+	{
+		$db = new PDO('mysql:host=localhost;dbname=blog_jean_forteroche;charset=utf8', 'root', '');
+	}
+	catch(Exception $e)
+	{
+		die('Erreur : '.$e->getMessage());
+	}
+
+	$req = $db->prepare('SELECT id, blog_post_title, blog_content, DATE_FORMAT(blog_post_date, \'%d/%m/%Y à %Hh%imin%ss\') AS blog_post_date_fr FROM blog_posts WHERE id = ?');
+	$req->execute(array($postId));
+	$post = $req->fetch();
+
+	return $post;
+}
+
+function getComments($postId)
+{
+	try
+	{
+		$db = new PDO('mysql:host=localhost;dbname=blog_jean_forteroche;charset=utf8', 'root', ''); 
+	}
+	catch(Exception $e)
+	{
+		die('Erreur : '.$e->getMessage());
+	}
+
+	 $comments = $db->prepare('SELECT id, id_post_blog, id_member, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE id_post_blog = ? ORDER BY comment_date DESC');
+	 $comments->execute(array($postId));
+
+	 return $comments;
+}
