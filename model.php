694a7c3c2b321e8308124e9cae3cf11b1d98644b
Oliver Toussaint <toussaint.olivier66@gmail.com>
2019-05-12 22:56:21 +0200

    new function to avoid repetition of the code

--- model.php
+++ model.php
@@ -1,33 +1,16 @@
 <?php
 function getPosts()
 {
- try
- {
-    $db = new PDO('mysql:host=localhost;dbname=blog_jean_forteroche;charset=utf8', 'root', '');
+ 	$db = dbConnect();
+    // on récupère les posts
+    $req = $db->query('SELECT id, blog_post_title, blog_content, DATE_FORMAT (blog_post_date, \'%d/%m/%Y à %Hh%imin%ss\') AS blog_post_date_fr FROM blog_posts ORDER BY id DESC LIMIT 0,5');
 
- }
- catch (Exception $e)
-{
-   // En cas d'erreur, on affiche un message et on arrête tout
-         die('Erreur : ' . $e->getMessage());
- }
-   // on récupère les posts
-       $req = $db->query('SELECT id, blog_post_title, blog_content, DATE_FORMAT (blog_post_date, \'%d/%m/%Y à %Hh%imin%ss\') AS blog_post_date_fr FROM blog_posts ORDER BY id DESC LIMIT 0,5');
-
-return $req;
+	return $req;
 }
 
 function getPost($postId)
 {
-	try
-	{
-		$db = new PDO('mysql:host=localhost;dbname=blog_jean_forteroche;charset=utf8', 'root', '');
-	}
-	catch(Exception $e)
-	{
-		die('Erreur : '.$e->getMessage());
-	}
-
+	$db = dbConnect();
 	$req = $db->prepare('SELECT id, blog_post_title, blog_content, DATE_FORMAT(blog_post_date, \'%d/%m/%Y à %Hh%imin%ss\') AS blog_post_date_fr FROM blog_posts WHERE id = ?');
 	$req->execute(array($postId));
 	$post = $req->fetch();
@@ -36,18 +19,23 @@ function getPost($postId)
 }
 
 function getComments($postId)
+{	
+	$db = dbConnect();
+	 $comments = $db->prepare('SELECT id, id_post_blog, id_member, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE id_post_blog = ? ORDER BY comment_date DESC');
+	 $comments->execute(array($postId));
+
+	 return $comments;
+}
+
+function dbConnect()
 {
-	try
+	try 
 	{
 		$db = new PDO('mysql:host=localhost;dbname=blog_jean_forteroche;charset=utf8', 'root', ''); 
+		return $db;
 	}
 	catch(Exception $e)
 	{
 		die('Erreur : '.$e->getMessage());
 	}
-
-	 $comments = $db->prepare('SELECT id, id_post_blog, id_member, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE id_post_blog = ? ORDER BY comment_date DESC');
-	 $comments->execute(array($postId));
-
-	 return $comments;
 }
