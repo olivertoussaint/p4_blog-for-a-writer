4b053be0bec762b14b15320cfec11278362d3899
Oliver Toussaint <toussaint.olivier66@gmail.com>
2019-05-21 01:01:09 +0200

    improvement page

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -7,7 +7,6 @@
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
       <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-      <link rel="stylesheet" href="./public/css/main_style.css" />
       <title><?= $title ?></title>
    </head>
    <body> 
