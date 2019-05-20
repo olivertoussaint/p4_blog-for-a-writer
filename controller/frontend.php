b9f60993bca5fa6e5e90708e81273dba32e3d7eb
Oliver Toussaint <toussaint.olivier66@gmail.com>
2019-05-21 01:04:52 +0200

    improvement page

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,18 +1,36 @@
 <?php
+ require_once('model/PostManager.php');
+ require_once('model/CommentManager.php');
 
-require('model/frontend.php');
-
-function listPosts()
-{
-    $posts = getPosts();
+ function listPosts()
+ {
+ 	$postManager = new PostManager(); // Création d'un objet
+ 	$posts = $postManager->getPosts();
 
     require('view/frontend/listPostsView.php');
 }
 
 function post()
 {
-    $post = getPost($_GET['id']);
-    $comments = getComments($_GET['id']);
+	$postManager = new PostManager(); // Création d'un objet
+	$commentManager = new CommentManager();
+
+    $post = $postManager->getPost($_GET['id']);
+    $comments = $commentManager->getComments($_GET['id']);
 
     require('view/frontend/postView.php');
+}
+
+function addComment($postId, $memberId, $comment)
+{
+	$postManager = new PostManager(); // Création d'un objet
+
+    $affectedLines = $commentManager->postComment($postId, $memberId, $comment);
+
+    if ($affectedLines === false) {
+        throw new Exception('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        header('Location: index.php?action=post&id=' . $postId);
+    }
 }
