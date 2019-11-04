e90fb6aee724f9237c5a17ea4b85312538feedd8
Oliver Toussaint <toussaint.olivier66@gmail.com>
2019-11-05 00:24:18 +0100

    ajout du controlleur backend

---
+++ controller/backend.php
@@ -0,0 +1,70 @@
+	<?php
+	require_once('model/PostManager.php');
+	require_once('model/CommentManager.php');
+	require_once('model/UserManager.php');
+
+	function administration()
+	{
+		$postManager = new \Blog\JeanForteroche\Model\PostManager();
+		$commentManager = new \Blog\JeanForteroche\Model\CommentManager();
+		$posts = $postManager->getPosts();
+		$comments = $commentManager->getReporting();
+		require('view/backend/dashboard.php');
+	}
+
+	function getColor($table, $colors)
+	{
+		if(isset($colors[$table])){
+			return $colors[$table];
+		} else {
+			return "orange";
+		}
+	}
+
+	function displayUpdate() 
+	{
+		$postManager = new \Blog\JeanForteroche\Model\PostManager();
+		$post = $postManager->getPost($_GET['id']);
+		require('view/backend/editPost.php');
+	}
+
+	function updatedPost($title, $content, $id) {
+		$postManager = new \Blog\JeanForteroche\Model\PostManager();	
+		$updatedPost = $postManager->updatePost($title, $content, $id);
+	}
+
+	function createPost() {
+		require('view/backend/createPost.php');
+	}
+
+	// function editPost() {
+	// 	require('view/backend/editPost.php');
+	// }
+
+	function newPost($title, $content)
+	{
+		$postManager = new \Blog\JeanForteroche\Model\PostManager();
+		$newPost = $postManager->createPost($title, $content);
+		header('Location: index.php?action=administration');
+	}
+
+	function removePost($postId)
+	{
+		$postManager = new \Blog\JeanForteroche\Model\PostManager();
+		$deletedPost = $postManager->deletePost($postId);
+
+	}
+
+	function removeComment($commentId)
+	{
+		$commentManager = new \Blog\JeanForteroche\Model\CommentManager();
+		$deletedComment = $commentManager->deleteComment($commentId);
+
+	}
+
+	function approveComment($postId)
+	{
+		$commentManager = new \Blog\JeanForteroche\Model\CommentManager();
+		$approvedComment = $commentManager->validatecomment($postId);
+	}
+
