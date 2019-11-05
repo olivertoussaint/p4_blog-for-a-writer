193a4698aba4c90ac73245d5a239cd2bf9250081
Oliver Toussaint <toussaint.olivier66@gmail.com>
2019-11-05 01:06:08 +0100

    mise à jour du routeur

--- index.php
+++ index.php
@@ -6,102 +6,124 @@ require('controller/backend.php');
 try {
     if (isset($_GET['action'])) {
         if ($_GET['action'] == 'listPosts') {
-            if(isset($_GET['page']) && is_numeric($_GET['page'])){
-        $page_num = listPosts($_GET['page']);
-    } else {
-        throw new Exception('oups!!!');
-    }
+            listPosts();
         
-        } elseif ($_GET['action'] == 'post') {
+        } 
+        elseif ($_GET['action'] == 'post') {
             if (isset($_GET['id']) && $_GET['id'] > 0) {
                 post();
-
-            } else {
+            } 
+            else {
                 throw new Exception('Aucun identifiant de billet envoyé');
             }
-        } elseif ($_GET['action'] == 'addComment') {
+            } 
+            elseif ($_GET['action'] == 'addComment') {
             if (isset($_GET['id']) && $_GET['id'] > 0) {
                 if (!empty($_SESSION['id']) && !empty($_POST['comment'])) {
                     addComment($_GET['id'], $_SESSION['id'], $_POST['comment']);
-                } else {
-                    throw new Exception('Tous les champs doivent être remplis !');
+                } 
+                else {
+                throw new Exception('Tous les champs doivent être remplis !');
                 }
             } else {
-                throw new Exception('Aucun identifiant de billet envoyé');
-            }
+              throw new Exception('Aucun identifiant de billet envoyé');
+            }  
+        } 
+        elseif ($_GET['action'] == 'register') {
+                    register();
+
         } elseif ($_GET['action'] == 'login') {
-            if (!isset($_SESSION['pseudo'])) {
-                login();
+            login();
 
-            } else {
-                header("Location: index.php");
-                throw new Exception('Vous êtes déja connecté !');
-            }
-        } elseif ($_GET['action'] == 'signup') {
-            signup();
-
-        } elseif ($_GET['action'] == 'register') {
-            register();
-
-        } elseif ($_GET['action'] == 'verifUser') {
-            verifUser();
-
-        } elseif ($_GET['action'] == 'createUser') {
-            createUser();
-
-        } elseif ($_GET['action'] == 'forgetPass') {
-            forgetPass();
-
-        } elseif ($_GET['action'] == 'resetPass') {
-            resetPass();
-
-        } elseif ($_GET['action'] == 'updatePass') {
-            updatePass();
-
-        } elseif ($_GET['action'] == 'logout') {
+        }  elseif ($_GET['action'] == 'addMember') {
+                        if (!empty($_POST['pseudo']) && !empty($_POST['email']) && !empty($_POST['password']) && !empty($_POST['password_confirm'])) {
+                                    if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+                                            if ($_POST['password'] == $_POST['password_confirm']) {
+                                                    addMember(strip_tags($_POST['pseudo']), strip_tags($_POST['password']), strip_tags($_POST['email']));
+                                    }
+                                    else {
+                                            throw new Exception('Les deux mots de passe ne correspondent pas.');
+                                    }
+                            } else {
+                                      throw new Exception('Pas d\'adresse email valide.');
+                            }
+                        } else {
+                                  throw new Exception('Tous les champs ne sont pas remplis !');
+                        }
+                } 
+                elseif ($_GET['action'] == 'loginSubmit') {
+                         loginSubmit(strip_tags($_POST['pseudo']), strip_tags($_POST['password']));
+                } 
+                elseif ($_GET['action'] == 'aboutMe') {
+                         aboutMe();
+        } 
+        elseif ($_GET['action'] == 'logout') {
             logout();
+        } 
+        elseif ($_GET['action'] == 'reporting') {
+            postReport($_GET['id'], $_SESSION['pseudo']);
+        } 
+        elseif ($_GET['action'] == 'validateComment') {
+            approveComment($_GET['id'], $_SESSION['pseudo']);
+        } 
+        elseif ($_GET['action'] == 'deleteComment') {
+            removeComment($_GET['id']);
+        } 
+        elseif ($_GET['action'] == 'administration') {
+                  if (isset($_SESSION['pseudo']) && ($_SESSION['role'] == '1')) {
+                          administration();
 
-        } elseif ($_GET['action'] == 'reporting') {
-            reporting();
+                  } else {
+                         throw new Exception('Vous n\'êtes pas autorisé à accéder à cette partie du site');
+                         header("Location: index.php");             
+                  } 
+        } 
+        elseif ($_GET['action'] == 'updatePost') {
+                  if (isset($_GET['id']) && $_GET['id'] > 0) {
+                        if (isset($_SESSION) && $_SESSION['role'] == '1') {
+                               displayUpdate();
+                        }  
+        } else {
+                        throw new Exception('Vous n\'êtes pas autorisé à accéder à cette partie du site');
+            }
+        }
 
-        } elseif ($_GET['action'] == 'administration') {
+        elseif ($_GET['action'] == 'displayUpdate') {
             if (isset($_SESSION['pseudo']) && ($_SESSION['role'] == '1')) {
-                administration();
+                displayUpdate();
 
             } else {
-                header("Location: index.php");
-                echo ('Vous n\'êtes pas autorisé à accéder à cette partie du site');
-            } 
+                    throw new Exception('Vous n\'êtes pas autorisé à accéder à cette partie du site');
+            }
+        }
 
-        } elseif ($_GET['action'] == 'newPost') {
-            newPost();
+        elseif ($_GET['action'] == 'postUpdate') {
+            if(isset($_GET['id'])&&($_GET['id'] > 0)) {
 
-        } elseif ($_GET['action'] == 'deletePost') {
-           if (isset($_SESSION['pseudo']) && ($_SESSION['role'] == '1')) {
-                deletePost();
-
-            } else {
-                header("Location: index.php");
-                echo ('Vous n\'êtes pas autorisé à accéder à cette partie du site');
-            }          
-                } elseif ($_GET['action'] == 'flaggedOff') {
-           if (isset($_SESSION['pseudo']) && ($_SESSION['role'] == '1')) {
-                flaggedOff();
+            updatedPost($_POST['post_title'], $_POST['mytextarea'], $_GET['id'] );
+            }
+        } 
+        elseif ($_GET['action'] == 'createPost') {
+            if (isset($_SESSION['pseudo']) && ($_SESSION['role'] == '1')) {
+                createPost();
 
             } else {
-                header("Location: index.php");
-                echo ('Vous n\'êtes pas autorisé à accéder à cette partie du site');
-            }          
-                } elseif ($_GET['action'] == 'editPost') {
-           if (isset($_SESSION['pseudo']) && ($_SESSION['role'] == '1')) {
-                editPost();
+                    throw new Exception('Vous n\'êtes pas autorisé à accéder à cette partie du site');
+            }
+        } 
+        elseif ($_GET['action'] == 'submitPost') {
+                if (!empty($_POST['title']) && !empty($_POST['mytextarea'])) {
+                      newPost($_POST['title'], $_POST['mytextarea']);
 
-            } else {
-                header("Location: index.php");
-                echo ('Vous n\'êtes pas autorisé à accéder à cette partie du site');
-            }                   
-             
-        } elseif ($_GET['action'] == 'mentionsLegales') {
+                } else {
+                        throw new Exception('Merci de remplir les champs demandés !');
+                }
+        } 
+        elseif ($_GET['action'] == 'deletePost') {
+                removePost($_GET['id']);
+         
+        }   
+        elseif ($_GET['action'] == 'mentionsLegales') {
             mentionsLegales();
 
         }
@@ -113,4 +135,4 @@ try {
 catch (Exception $e) {
     $errorMessage = $e->getMessage();
     require('view/frontend/errorView.php');
-    }
+}
