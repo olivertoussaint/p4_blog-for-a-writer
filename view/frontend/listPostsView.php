f0a292be167b261671db7a0473b3a5c42bb2f4f4
Oliver Toussaint <toussaint.olivier66@gmail.com>
2019-11-05 01:18:11 +0100

    mise à jour des posts frontend

--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -1,102 +1,71 @@
-<!DOCTYPE html>
-<html lang="fr">
-   <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-      <link rel="stylesheet" href="./public/css/main_style.css" />
-      <?php $title ='Personnal Blog of Jean Forteroche'; ?>
-   </head>
-   <body>
-      <div id="home">
-         <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-            <div class="container-fluid">
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-main">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand-align" href="#"><img src="./public/images/Logo_projet_4.png"></a>
-                </div>
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="navbar-collapse-main">
-                    <ul class="nav navbar-nav">
-                        <li><a class="active" href="index.php"><i class="fa fa-home fa-lg" aria-hidden="true"></i> Home<span class="sr-only">(current)</span></a></li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-                      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                   </ul>
-                </div>
-                <!-- /.navbar-collapse -->  
-            </div>
-            <!-- /.container-fluid -->     
-        </nav>
-         <?php ob_start(); ?>
-         <div class="landing-text">
-            <h1 class="main-title">Billet simple pour L'Alaska</h1>
-         </div>
-      </div>
-      <div class="container-post">
-         <div class="row">
-            <h2 class="last_chapters">Les derniers épisodes</h2>
-            <hr class="separate">
-            <div class="col-sm-6 col-sm-offset-3">
-            <?php            
-               while ($data = $posts->fetch())       
-               {
-            ?>
-            <div class="news">
-               <h3>
-                  <?= htmlspecialchars($data['blog_post_title']) ?>
-                  <em>le <?= $data['blog_post_date_fr'] ?></em>
-               </h3>
-               <p>
-                  <?= nl2br(htmlspecialchars($data['blog_content'])) ?>
-                  <br />
-                  <br />
-                  <strong><a href="index.php?action=post&amp;id=<?= $data['id'] ?>" class="btn btn-success">Lire la suite</a></strong>
-               </p>
-            
-            <div class="comment">
-               <em><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Commentaires</a></em>
-            </div>
-            </div>
-            <?php
-               }
-               $posts->closeCursor();
-            ?>
-         </div>
-         </div>
-      </div>
-      <?php $content = ob_get_clean(); ?>
+  <?php
+    $title = 'Billet simple pour l\'Alaska - Jean Forteroche';
+    ?> 
 
-      <?php require('template.php'); ?>
-
-      <footer class="container-fluid text-center">
+      <?php
+    ob_start();
+    ?>
+          <div class="container">
             <div class="row">
-                <div class="col-sm-4">
-                    <h3>Mentions Légales</h3>
-                </div>
-                <div class="col-sm-4" class="social-icons">
-                    <h3>Restons connecté</h3>
-                    <a href="#" class=" fa fa-facebook fa-2x"></a>
-                    <a href="#" class=" fa fa-twitter fa-2x"></a>
-                    <a href="#" class=" fa fa-google fa-2x"></a>
-                    <a href="#" class=" fa fa-linkedin fa-2x"></a>
-                </div>
-                <div class="col-sm-4">
-                    <h3>Me contacter</h3>
-                </div>
+              <div id="main-title">
+                  <h1 class="title">Un billet simple pour l'Alaska</h1>
+              </div>
+                  <div class="card-panel blue lighten-5">
+                    <p >
+                      <h5 id="author-header-preface">Chers lecteurs, Chères lectrices,</h5>   
+                      <h6 id="author-preface">Je vous propose de me suivre en ligne pour la rédaction de mon tout nouveau roman.<br />
+                              Toutes suggestions et critiques constructives pour ce beau projet seront les bienvenues.</h6>
+                    </p>
+                  </div>
             </div>
-        </footer>
-        <button onclick="topFunction()" id="myBtn" title="Haut de page"><i class="fa fa-arrow-up fa-2x" aria-hidden="true"></i></button>
-        <script src="./public/js/scrollToTop.js"></script>
-   </body>
-</html>
+          </div>
+
+          <div id="chapters" class="container row">
+
+    <?php
+    while ($data = $posts->fetch()) {
+    ?>
+
+          <div class="col s12 m6 l6 xl4">
+              <div class="card z-depth-3">
+                  <div class="card-image waves-effect waves-block waves-light">
+                    <img class="activator" src="./public/uploads/<?= ($data['image']) ?>" alt="<?= htmlspecialchars($data['post_title']) ?>" />
+                  </div>
+          <div class="card-content">
+              <h5 class="grey-text text-darken-2">Chapitre&nbsp;&nbsp;:&nbsp;<strong><?= htmlspecialchars($data['post_title']) ?></strong></h5>  
+              <h6 class="post-date">publié le <?= $data['post_date_fr'] ?></h6>
+              <h6 class="right">par :&nbsp; Jean Forteroche</h6> 
+          </div>      
+          <div class="card-content"><hr />
+              <span class="card-title activator grey-text text-darken-4"><i class="material-icons right">more_vert</i></span>
+              <p><a href="index.php?action=post&amp;id=<?= $data['id'] ?>" class="waves-effect waves-light btn">lire le chapitre</a></p>
+          </div>
+          <div class="card-reveal">
+              <span class="card-title grey-text text-darken-4"><?= htmlspecialchars($data['post_title']) ?><i class="material-icons right" >close</i></span>
+                  <p>
+                        <?php
+                            $lg_max       = 850;
+                            $blog_content = $data['blog_content'];
+                            $blog_content = strip_tags($blog_content);
+                            if (strlen($blog_content) > $lg_max) {
+                            $blog_content = substr($blog_content, 0, $lg_max);
+                            $last_space   = strripos($blog_content, " ");
+                            $blog_content = substr($blog_content, 0, $last_space) . " [...]";
+                        }      
+                          echo $blog_content;     
+                        ?>
+                  </p>
+          </div>
+              </div>                                   
+          </div>
+      <?php 
+          }   
+            $posts->closeCursor();
+      ?>         
+          </div>
+      <?php
+            $content = ob_get_clean();
+      ?>              
+      <?php
+        require('view/frontend/template.php');
+      ?> 
