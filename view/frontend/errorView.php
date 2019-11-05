12b1a5ae3bc6350409c9ea53c68ba249d521f567
Oliver Toussaint <toussaint.olivier66@gmail.com>
2019-11-05 20:57:30 +0100

    MAJ de la Vue backend

---
+++ view/frontend/errorView.php
@@ -0,0 +1,17 @@
+	<?php 
+	$title = 'Page 404 Erreur'; 
+	?>
+
+	<?php ob_start(); ?>
+	<div class="jumbotron text-center">
+	<h1 class="error">Oups... Une erreur s'est produite !</h1>
+	<img class="img-responsive img-error" src="public/images/img-404-error.PNG"
+	     alt="Image d'erreur 404 page n'existe pas." />
+	<br />
+	<h4><a href="index.php" title="Accueil">Revenir à l'accueil du site</a></h4>
+
+
+	<?php $content = ob_get_clean(); ?>
+	</div>
+
+	<?php require('view/frontend/template_3.php'); ?>
---
+++ view/frontend/template_2.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+   <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+      <meta name="description" content="Jean Forteroche, écrivain et acteur, travaille sur son prochain roman, Un Billet simple pour l'Alaska et le publie par épisode en ligne" />
+
+      <link rel="canonical" href="https://oliviertoussaint.fr/projet4/" />
+      <meta property="og:locale" content="fr_FR" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Jean Forteroche | Écrivain & acteur - Projet OpenClassrooms - Olivier Toussaint" />
+      <meta property="og:description" content="Jean Forteroche, écrivain et acteur, travaille sur son prochain roman, Un Billet simple pour l'Alaska et le publie par épisode en ligne" />
+      <meta property="og:url" content="https://oliviertoussaint.fr/projet4/" />
+      <meta property="og:site_name" content="Jean Forteroche" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:description" content="Jean Forteroche, écrivain et acteur, travaille sur son prochain roman, Un Billet simple pour l'Alaska et le publie par épisode en ligne" />
+      <meta name="twitter:title" content="Jean Forteroche | Écrivain & acteur - Projet OpenClassrooms - Olivier Toussaint" />
+      <link rel="stylesheet" href="./public/css/style.css" />
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="./public/css/assets/materialize.css"  media="screen,projection"/>   
+      
+      <title><?= $title ?></title>
+   </head>
+<body>
+
+    <div>
+      <?= $content ?>   
+    </div>
+
+<?php require('public/template/footer.php'); ?>
+
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+      <script type="text/javascript" src="./public/js/assets/materialize.js"></script>   
+</body>
+</html>
