c289c01c216219202b7600acb82d03bb158c4b49
Oliver Toussaint <toussaint.olivier66@gmail.com>
2019-11-05 01:20:23 +0100

    mise à jour du post frontend

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -1,73 +1,143 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-        <link rel="stylesheet" href="./public/css/style.css" />
-        <?php $title ='Commentaires'; ?>
-    </head>
-        
-    <body>
-        <div class="container-fluid">
-        <div class="row">
-        <?php ob_start(); ?>
-    <p class="backHome"><a href="index.php" class="btn btn-primary"  >Retour à la liste des billets</a></p>
-    </div>
-    </div>
-    <div class="container">
-        <div class="row">       
-    <h3>Article</h3>
-    <hr>
-        <div class="news">
-            <h3>
-                <?= htmlspecialchars($post['blog_post_title']) ?>
-                <em>le <?= $post['blog_post_date_fr'] ?></em>
-            </h3>
-            <hr class="element-1">
-            <p class="post">
-                <?= nl2br(htmlspecialchars($post['blog_content'])) ?>
-            </p>
+  <?php $title ='Commentaires';
+   ?>
+  <?php ob_start(); ?>
+      <div class="row backhome">
+        <div class="col s12">
+          <p class="backhome-button">
+            <a href="index.php" class="btn btn-primary"  >Retour à la liste des billets</a>
+          </p>
         </div>
-        <hr class="element-1">
-        <h2>Espace des commentaires</h2>
-        <hr class="element-2">
-        <h4><i class="fa fa-comments" aria-hidden="true"></i> Commentaires</h4>
+      </div>  
+      <div class="container">
+        <div class="row">
+            <h4 class="chapter-title">
+              <p class=" center-align">
+              Chapitre&nbsp;:&nbsp;
+              <?= $post['post_title'] ?>
+            </h4>
+            <hr />
+              <div class="news">
+                <em> mis en ligne le <?= $post['post_date_fr'] ?></em>
+              </div>
+            <hr class="element-1">
+          <div class="post-wrapper">
+              <p class="post">
+                    <?= nl2br(htmlspecialchars($post['blog_content'])) ?>
+              </p>
+                <p class="signature">Jean Forteroche</p>       
+          </div>
+        </div>
+      </div>
+        <div class="container comments-field">
+          <div class="comment-space">
+          <h4 class="comment-space-title">Espace des commentaires</h4>
 
-        <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
-            <div>
-                <label for="author">Auteur</label><br />
-                <input type="text" id="author" name="author" />
-            </div>
-            <div>
-                <label for="comment">Commentaire</label><br />
-                <textarea id="comment" name="comment" rows="4" cols="50" placeholder="Merci de laisser votre commentaire ici ..."></textarea>
-            </div>
-            <div>
-                <input type="submit" value="Soumettre votre commentaire" />
-            </div>
-        </form>
-
-        <?php
+          <?php $nbComment = $nbComments->fetch();?>
+          <h5 class="comment-number"><i class="fa fa-comments" aria-hidden="true"></i>
+            <?= htmlspecialchars($nbComment['nombreComments']) ?> Commentaire<?php if ($nbComment['nombreComments'] >1) echo 's';?>
+          </h5>
+          <p class="no-comment">
+            <?php if ($nbComment['nombreComments'] ==0) echo "Aucun commentaire n'a été publié... Soyez le premier !";?>
+          </p>
+          </div>
+        </div>
+  <?php 
         while ($comment = $comments->fetch())
-        {
-        ?>
-        <hr>
-        <div class="comment-content">
-            <p><strong><?= htmlspecialchars($comment['id_member']) ?></strong> a écrit le : <?= $comment['comment_date_fr'] ?></p>
-            <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
-        </div>    
-        <?php
+        if (isset($_SESSION['pseudo']))
+          { ?>
+
+        <p class="comment-user">
+          <strong><?= htmlspecialchars($comment['pseudo']) ?></strong>
+        </p>
+        <p class="comment-date">
+            a écrit le : <?= $comment['comment_date_fr'] ?>
+        </p>
+        <p class="comment-comment">
+            <?= nl2br(htmlspecialchars($comment['comment'])) ?>
+        </p>
+          <div>
+            <p>
+              <a class="modal-trigger right" href="index.php?action=reporting&id=<?= $comment['id'] ?>">Signaler le commentaire</a>
+            </p>
+            <br />
+            <hr />
+            <div id="modal1" class="modal">
+              <div class="modal-content">
+                <h4> Signalement d'un commentaire </h4>
+              <p>
+                 Êtes vous sûr de vouloir signaler ce commentaire à notre administrateur ?
+              </p>
+              <div class="modal-footer">
+                <a class="modal-close btn-flat">Annuler</a>
+                <a href="index.php?action=reporting&id=<?= $comment['id'] ?> "class="modal-close btn-flat">Confirmer</a>
+              </div>
+          </div>
+        </div>
+        </div>
+  <?php 
+        }
+          ?>
+
+  <?php if(!empty($_SESSION))
+        { 
+          $id = $_GET['id'];
+          if(strpos($id, "#modal1")!=false) {
+
+            $id = substr($id, 0,strpos($id, "#modal1"));
+          }
+
+          ?>
+        <div class="container">
+          <div class="comments-form" >
+            <h4 class="left">Ajouter un commentaire : </h4>
+            <br />
+            <form action="index.php?action=addComment&amp;id=<?php echo $id ?>" method="post">
+              <label for="content">Commentaire : </label>
+              <br />
+              <textarea id="comment" name="comment" class="form-control" placeholder="Saisir votre commentaire ici..."></textarea>
+              <br />
+              <button class="btn btn-primary">Publier</button>
+            </form>
+          </div>
+        </div>
+        <br />
+
+  <?php
         }
         ?>
-        <?php $content = ob_get_clean(); ?>
-
-        <?php require('template.php'); ?>
-
       </div>
-    </div>
-    </body>
-</html>
+
+  <?php if(isset($_SESSION) && empty($_SESSION)) 
+        {
+      ?>
+
+      <div class="container">
+        <div class="row comment-chapter">
+          <hr />
+          </div>
+          <h4 class="comment-post-title center-align">Commentez ce chapitre</h4>
+          <div class="text-center text-info">
+            <p class="center-align">
+              Afin d'afficher le(s) commentaire(s) et de commenter ce chapitre. Connectez vous !        
+            </p>
+            <p class="center-align">
+              <a class="waves-effect waves-light btn-large" href="index.php?action=login" role="button">Loggin</a>
+              <a class="waves-effect waves-light btn-large" href="index.php?action=signup" role="button">Register</a>
+            </p>
+
+          </div>
+        
+      </div>
+
+  <?php
+        } 
+      ?>
+      </div>
+      <?php 
+      $content = ob_get_clean(); 
+  ?>
+
+  <?php 
+      require('view/frontend/template_2.php'); 
+  ?>
+
