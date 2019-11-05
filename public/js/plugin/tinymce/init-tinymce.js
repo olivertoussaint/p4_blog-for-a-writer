7873f3cb2afd8c06d1898b32fe02aa697739bcc7
Oliver Toussaint <toussaint.olivier66@gmail.com>
2019-11-05 01:37:55 +0100

    ajout des fichiers css + images + javascript

---
+++ public/js/plugin/tinymce/init-tinymce.js
@@ -0,0 +1,15 @@
+tinymce.init({
+	
+	  selector: '#mytextarea',
+    language : 'fr_FR',
+    width : 900,
+	  height: 500,
+    plugins: [
+    	'advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker',
+  		'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+  		'save table contextmenu directionality emoticons template paste textcolor'
+	 ],
+    content_css: 'css/content.css',
+    toolbar: 'insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons'
+
+});
