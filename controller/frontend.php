08638418190797fb1b0f2e2c90e82283fe48c5af
Oliver Toussaint <toussaint.olivier66@gmail.com>
2019-11-05 01:03:52 +0100

    mise à jour du controlleur frontend

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,60 +1,120 @@
-<?php
- require_once('model/PostManager.php');
- require_once('model/CommentManager.php');
- require_once('model/RegisterManager.php');
+    <?php
+    require_once('model/PostManager.php');
+    require_once('model/CommentManager.php');
+    require_once('model/UserManager.php');
 
- function listPosts()
- {
- 	$postManager = new PostManager(); // Création d'un objet
- 	$posts = $postManager->getPosts();
-
-    require('view/frontend/listPostsView.php');
-}
-
-function post()
-{
-	$postManager = new PostManager(); // Création d'un objet
-	$commentManager = new CommentManager();
-
-    $post = $postManager->getPost($_GET['id']);
-    $comments = $commentManager->getComments($_GET['id']);
-
-    require('view/frontend/postView.php');
-}
-
-function addComment($postId, $memberId, $comment)
-{
-	$postManager = new PostManager(); // Création d'un objet
-
-    $affectedLines = $commentManager->postComment($postId, $memberId, $comment);
-
-    if ($affectedLines === false) {
-        throw new Exception('Impossible d\'ajouter le commentaire !');
+    function listPosts()
+    {
+        $postManager = new \Blog\JeanForteroche\Model\PostManager();
+        $posts = $postManager->getPosts();
+        
+        require('view/frontend/listPostsView.php');
     }
-    else {
-        header('Location: index.php?action=post&id=' . $postId);
+
+    function post()
+    {
+        $postManager = new \Blog\JeanForteroche\Model\PostManager();
+        $commentManager = new \Blog\JeanForteroche\Model\CommentManager();  
+        $post = $postManager->getPost($_GET['id']);
+        $comments = $commentManager->getComments($_GET['id']);
+        $nbComments = $commentManager->countNumberComments($_GET['id']);
+        $posts = $postManager->getPosts();
+        require('view/frontend/postView.php');
     }
-}
 
-function signUp($pseudo, $email, $pass_hache)
-{
-	 $registerManager = new RegisterManager();
-	 $newMember = $registerManager->newMemberLogin($pseudo, $email, $pass_hache);
-	
-	 require('view/frontend/registerView.php');
-}
+    function addComment($postId, $memberId, $comment)
+    {
+        $commentManager = new \Blog\JeanForteroche\Model\CommentManager();
+        $affectedLines  = $commentManager->postComment($postId, $memberId, $comment);
+        if ($affectedLines === false) {
+            throw new Exception('Impossible d\'ajouter le commentaire !');
+        } else {
+            header('Location: index.php?action=post&id=' . $postId);
+        }
+        
+        require('view/frontend/postView.php');
+    }
 
-function authentification()
-{
-	$authentificationManager = new AuthentificationManager();
-	$login = $authentificationManager->CheckUser();
+    function postReport($postId)
+    {
+      $commentManager = new \Blog\JeanForteroche\Model\CommentManager();
+      $reportedComment = $commentManager->reporting($postId);
+    }
 
-	require('view/frontend/connectView.php');
-}
+    function login()
+    {
+        require('view/frontend/loginView.php');
+    }
 
-function signupForm()
-{
+    function register()
+    {
+        require('view/frontend/registerView.php');
+    }
 
- require('view/frontend/registerView.php');
-	
-}
+    function addMember($pseudo, $pass, $mail) 
+    {
+        $userManager = new \Blog\JeanForteroche\Model\UserManager();
+        $pseudoChecked = $userManager->checkPseudo($pseudo);
+        $emailChecked = $userManager->checkMail($mail);
+        if ($pseudoChecked) {
+            header('Location: index.php?action=subscribe&error=invalidUsername');   
+        }
+        if ($emailChecked) {
+            header('Location: index.php?action=subscribe&error=invalidMail');
+        }
+        if (!$pseudoChecked && !$emailChecked) {
+            $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);            
+            $newMember = $userManager->createMember($pseudo, $mail, $pass);
+        }   
+    } 
+
+    function loginSubmit($pseudo, $pass) 
+    {
+        $userManager = new \Blog\JeanForteroche\Model\UserManager(); 
+        $member = $userManager->loginMember($pseudo);
+        $isPasswordCorrect = password_verify($_POST['password'], $member['password']);
+        if (!$member) {
+            header('Location: index.php');
+        } else {
+         if ($isPasswordCorrect) {
+            $_SESSION['id'] = $member['id'];
+            $_SESSION['pseudo'] = ucfirst(strtolower($pseudo));
+            $_SESSION['role'] = $member['role'];
+            header('Location: index.php');
+            echo 'Bienvenue ' . $_SESSION['pseudo'];
+        } else {
+            header('Location: index.php');
+        }
+    }
+    }
+
+    function aboutMe()
+    {
+        require('view/frontend/aboutMe.php');
+    }
+
+    function logout()
+    {
+        session_start();
+        $_SESSION = array();
+        session_destroy();
+        header("Location: index.php");
+    }
+
+    function countComments($postId)
+    {
+        $commentManager = new \Blog\JeanForteroche\Model\CommentManager(); 
+        $nbrComments = $commentManager->countNumberComments($postId);
+        
+        require('view/frontend/postView.php');
+    }
+
+    function mentionsLegales()
+    {
+        require('view/frontend/mentionsLegales.php');
+    }
+
+    function error()
+    {
+        require('view/frontend/errorView.php');
+    }
